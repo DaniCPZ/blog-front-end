1fa80c0ff4b6f24d82f2926e6fe8ddb35d6d0563
Daniel Valenzuela <danielvalenzuela199825@gmail.com>
2022-02-28 20:43:55 -0300

    Articles section design

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <Hero />
+  <div>
+    <Hero />
+    <div class="py-8 sm:py-20 px-4 sm:px-0 container mx-auto">
+      <Articles />
+    </div>
+  </div>
 </template>
 
 <script>
+import Articles from "../../components/Articles.vue";
 import Hero from "./Hero.vue";
 export default {
   name: "Home",
-  components: { Hero },
+  components: { Hero, Articles },
 };
 </script>
