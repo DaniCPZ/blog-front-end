ca1a6c1e45a5775b3bc2a645a9a65261cc130e8b
Daniel Valenzuela <danielvalenzuela199825@gmail.com>
2022-02-28 21:27:13 -0300

    Contact page design

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,12 @@ const routes = [
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About/index.vue')
-  }
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact/index.vue')
+  },
 ]
 
 const router = createRouter({
